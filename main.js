e0a3efecc9bdfe133c0e03ae2060fa7c5ed2403a
Miguel Garcia <miguelgarcia2515@gmail.com>
2015-04-29 08:48:41 -0700

    YES finished the test

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.dan').click(function() {
+        
+        alert("Hey you clicked me!");
+    });
+});
+
+$(document).ready(function() {
+    $('#iwao').click(function() {
+        $('#iwao').hide();
+      
+    });
+});
+
+
